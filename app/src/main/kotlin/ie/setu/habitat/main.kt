bd1d136af6dc7b3fb6a9213aa6e7a64ba913cc19
Isadora Fitzgerald <33418972+fitzdora@users.noreply.github.com>
2023-09-30 12:11:11 +0100

    Adding menu functionality

--- app/src/main/kotlin/ie/setu/habitat/main.kt
+++ app/src/main/kotlin/ie/setu/habitat/main.kt
@@ -7,4 +7,23 @@ fun main(args: Array<String>) {
     logger.info { "Launching Habitat Console App" }
     println("Habitat Kotlin App Version 1.0")
 
+    var input: Int
+    input = menu()
+
+}
+
+fun menu() : Int {
+
+    var option : Int
+    var input: String? = null
+
+    println("Main Menu")
+    println(" 1. Add Habitat")
+    println("-1. Exit")
+    println()
+    print("Enter an integer : ")
+    input = readLine()!!
+    option = input.toInt()
+
+    return option
 }
