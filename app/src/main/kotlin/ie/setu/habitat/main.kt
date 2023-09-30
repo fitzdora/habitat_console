541d35ed232671918c2a4dcad74734687e51f79a
Isadora Fitzgerald <33418972+fitzdora@users.noreply.github.com>
2023-09-30 12:30:47 +0100

    Extending menu functionality

--- app/src/main/kotlin/ie/setu/habitat/main.kt
+++ app/src/main/kotlin/ie/setu/habitat/main.kt
@@ -8,8 +8,19 @@ fun main(args: Array<String>) {
     println("Habitat Kotlin App Version 1.0")
 
     var input: Int
-    input = menu()
 
+    do {
+        input = menu()
+        when(input) {
+            1 -> addHabitat()
+            2 -> updateHabitat()
+            3 -> listHabitat()
+            -1 -> exitHabitatMenu()
+            else -> println("Invalid Option")
+        }
+        println()
+    } while (input != -1)
+    logger.info { "Shutting Down Habitat Console App" }
 }
 
 fun menu() : Int {
@@ -19,11 +30,30 @@ fun menu() : Int {
 
     println("Main Menu")
     println(" 1. Add Habitat")
+    println(" 2. Update Habitat")
+    println(" 3. List All Habitats")
     println("-1. Exit")
     println()
     print("Enter an integer : ")
     input = readLine()!!
-    option = input.toInt()
-
+    option = if (input.toIntOrNull() != null && !input.isEmpty())
+        input.toInt()
+    else
+        -9
     return option
+}
+
+fun addHabitat(){
+    println("You chose to add a Habitat")
+}
+fun updateHabitat(){
+    println("You chose to update a Habitat")
+}
+
+fun listHabitat(){
+    println("You chose to List all Placemarks")
+}
+
+fun exitHabitatMenu(){
+    println("Exiting Habitat Menu")
 }
